2030b25ee9643e62531ea5ebde09c501a6047cfc
andreilemisz <andreilemisz@gmail.com>
2024-08-30 12:35:51 -0300

    Adicionadas as tarefas na visualização do HTML >> >> Consegui utilizar uma função genérica do Django chamada ListView que já mostra os itens >> do banco de dados como uma lista. Agora falta adicionar os botões para edição desses >> dados dentro do próprio browser. Também falta melhorar a disposição dos elementos de >> frontend.

--- gestortarefas/urls.py
+++ gestortarefas/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from gestortarefas.views import lista_todo_visualizacao
 
 app_name = "gestortarefas"
 
@@ -7,7 +8,6 @@ urlpatterns = [
     # path('<int:contact_id>/', contact_log_views.contact, name='contact'), -> Transformar em TarefaID
     # path("search/", contact_log_views.contact_log_search, name="search"), -> Para página de Search
                                                 # Precisa de uma URL separada para search ou só filtro?
-    path("teste/", views.gestortarefas_paginateste, name="gestortarefas_paginateste"),
-    path("mobile/", views.gestortarefas_paginamobile, name="gestortarefas_paginamobile"),
-    path("", views.gestortarefas_paginainicial, name="gestortarefas_paginainicial"),
+    path("", lista_todo_visualizacao.as_view()),
+    # path("", views.lista_todo_visualizacao, name="lista_todo_visualizacao"),
     ]
--- gestortarefas/views.py
+++ gestortarefas/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from gestortarefas.models import Tarefa
 
-def gestortarefas_paginateste(request):
-    
-    # tarefas = Tarefa.objects.all().order_by("prioridade_tarefa")
-    # context_object_name = "conjunto_tarefas"
-    
-    return render(request, 'gestortarefas/base.html')
 
-def gestortarefas_paginamobile(request):
-    
-    # tarefas = Tarefa.objects.all().order_by("prioridade_tarefa")
-    # context_object_name = "conjunto_tarefas"
-    
-    return render(request, 'gestortarefas/mobile.html')
+class lista_todo_visualizacao(ListView):
+    model = Tarefa
+    template_name = "gestortarefas/paginainicial.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context    
+
+""" 
+Versão antiga com request
 def gestortarefas_paginainicial(request):
     
-    # tarefas = Tarefa.objects.all().order_by("prioridade_tarefa")
-    # context_object_name = "conjunto_tarefas"
+    tarefas = Tarefa.objects.all().order_by("prioridade_tarefa")
+    context_object_name = "conjunto_tarefas"
     
     return render(request, 'gestortarefas/paginainicial.html')
+    """
 # Create your views here.
