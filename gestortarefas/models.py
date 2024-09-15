367013f5b2b390094b93d9462386076ba6bdf7f4
andreilemisz <andreilemisz@gmail.com>
2024-09-15 14:27:31 -0300

    Feita importação das tarefas relativas ao usuário na tela principal quando o login é realizado corretamente. As tarefas aparecem em ordem, com uma checkbox do lado. Desenhei a estrutura que possibilitou essa visualização no arquivo de Histórico, porque o caminho envolveu muito código. Também arrumei o problema da não importação do CSS dentro da tela principal.

--- gestortarefas/models.py
+++ gestortarefas/models.py
@@ -5,7 +5,7 @@ class Tarefa(models.Model):
     nome_tarefa = models.CharField(max_length=100)
     status_tarefa = models.BooleanField(default=False)
     descricao_tarefa = models.TextField(blank=True)
-    usuariovinculado = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
+    usuariovinculado = models.ForeignKey(User, on_delete=models.CASCADE)
     # data_vencimento = ?                               -> Como fazer?
     # lembrete_tarefa = ?                               -> Como fazer?
     # prioridade_tarefa = models.BooleanField(default=False) -> implementar futuramente
--- gestortarefas/urls.py
+++ gestortarefas/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
+from django.urls import path # type: ignore
 from gestortarefas.views import lista_todo_visualizacao
 
 app_name = "gestortarefas"
 
-urlpatterns = [
-    # path('<int:contact_id>/', contact_log_views.contact, name='contact'), -> Transformar em TarefaID
-    # path("search/", contact_log_views.contact_log_search, name="search"), -> Para página de Search
+#urlpatterns = [
+        # path('<int:contact_id>/', contact_log_views.contact, name='contact'), -> Transformar em TarefaID
+        # path("search/", contact_log_views.contact_log_search, name="search"), -> Para página de Search
                                                 # Precisa de uma URL separada para search ou só filtro?
-    path("", lista_todo_visualizacao.as_view()),
-    # path("", views.lista_todo_visualizacao, name="lista_todo_visualizacao"),
-    ]
+#path("", lista_todo_visualizacao.as_view()),
+        # path("", views.lista_todo_visualizacao, name="lista_todo_visualizacao"),
+#    ]
